8202c0431971ee966aba4df56ee23d995786b149
Fernando <fernandoamrein@gmail.com>
2024-08-25 18:21:14 -0300

    modified plot_rc.py

--- plot_rc.py
+++ plot_rc.py
@@ -2,19 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-datos = ["c1","c2","11","12","21","22"]
-for set_de_datos in datos:
+def makePlot(path):
 
-    # Number of files
-    start = 1
-    num_files = 43 
-
-    # Create a plot
-    plt.figure(figsize=(12, 8))
+    figure, ax = plt.subplots()
 
     # Loop through each file
-    for i in range(start, num_files+1):
-        filename = "r"+set_de_datos+"_"+str(i)+".dat"
+    for i in range(15):
+        filename = f"../{path}/us_corrida1/restraint_{str(i+1)}.dat"
         if os.path.isfile(filename):    
             # Load data from file
             data = np.loadtxt(filename)
@@ -25,14 +19,18 @@ for set_de_datos in datos:
 
                 # Plot the second column
                 plt.plot(x, y, alpha=0.7)
+                ax.set_ylim(7, 25)
 
     # Labeling
-#    plt.xlabel('First Column')
-#    plt.ylabel('Second Column')
-    plt.title(set_de_datos)
+    #    plt.xlabel('First Column')
+    #    plt.ylabel('Second Column')
+    #plt.title(set_de_datos)
     #plt.legend(loc='upper right', bbox_to_anchor=(1.2, 1))  # Adjust legend position
 
     # Show plot
-#    plt.tight_layout()  # Adjust layout to make room for the legend
-    plt.show()
-    plt.close()
+    #    plt.tight_layout()  # Adjust layout to make room for the legend
+    plt.savefig(f"../{path}/us_corrida1/rc.png")
+    #plt.close()
+
+makePlot("ATP")
+makePlot("apo")
